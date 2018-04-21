563fe53b1c5c9e2d84320fc8cdf02b6a0cd152b3
Will Cheng <willchengsters@gmail.com>
2018-04-21 16:14:14 -0700

    forgot to import react

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
-
+import React from 'react';
 
 const App = () => (
     <div>
         <div className="app">
-            <img src={logo} className="logo rotate"/>
+            <img className="logo rotate"/>
             <h1>BeachHacks2018</h1>
         </div>
     </div>
