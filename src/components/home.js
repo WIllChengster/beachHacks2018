9966222a324b3424ceaae2d0e253abf3a4bc9951
Will Cheng <willchengsters@gmail.com>
2018-04-22 01:24:37 -0700

    epxlore

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+class Home extends Component {
+    constructor(props){
+        super(props)
+    }
 
-export default () => {
-    return(
-        <div>
-            <div className="row">
-                <h1 className="text-center display-3 mb-5 w-100">Welcome to the Home Page</h1>
-                <h3 className="display-4 text-center w-100">1,405,230 hackers </h3>
-                <input type="text" className="form-control my-5" placeholder="search for a hackathon"/>
+    render(){
+        const hackathon={
+            height: '100px',
+            width: '150px',
+            backgroundColor:'lightGray',
+            display: "inline-block",
+            margin: '5px'
+        }
+        const center={
+            
+        }
+        return(
+            <div>
+                <div className="">
+                    <h1 className="text-center display-3 mb-5 w-100">Welcome to the Home Page</h1>
+                    <h3 className="display-4 text-center w-100">1,405,230 hackers </h3>
+                    <input type="text" className="form-control my-5" placeholder="search for a hackathon"/>
+                </div>
+                    <h4 className="my-5" >Explore Hackathons</h4>
+                    <div className="">
+                        <div style={hackathon}>
+                            <h6>Hackathon Title</h6>
+                        </div>
+                        <div style={hackathon}>
+                            <h6>Hackathon Title</h6>
+                        </div>
+                        <div style={hackathon}>
+                            <h6>Hackathon Title</h6>
+                        </div>
+                    </div>
+            
             </div>
-            <div className="row">
-                <h1>Explore Hackathons</h1>
-                
-            </div>
-        
-        </div>
-    )
+        )
+    }
 }
+
+export default Home
 
