4216ee530c4ec4c46d9c90532b2483a37a93e080
Will Cheng <willchengsters@gmail.com>
2018-04-21 22:21:54 -0700

    completed basic layout

--- src/components/eventPage/index.js
+++ src/components/eventPage/index.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class EventPage extends Component{
     constructor(props){
@@ -47,23 +48,28 @@ class EventPage extends Component{
 
         return(
             <div className="row w-100 mb-5">
-                <div style={dummyImg} className="text-center" > dummy img </div>
+                <div style={dummyImg} className="text-center mt-5" > dummy img </div>
                 <div className="row w-100">
                     <h1 className="text-center w-100 my-3 " >Title of Hackathon</h1>
                 </div>
                 <div className="row">
                     <div className="col-4">
-                        <div  style={desc} >
-                            <h3 className="text-center" >Description</h3>
-                            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum consequuntur tempora corrupti dolores expedita sequi magnam quasi maiores neque eaque aliquam nulla, facilis pariatur, quidem eos ullam nisi dicta officia libero voluptate quibusdam dolor adipisci ut? Alias nihil, in iusto incidunt earum tenetur, nam quisquam ipsa corrupti sint et ad enim reprehenderit neque, dicta quis eveniet natus ducimus cupiditate molestias facilis sit repellat ex pariatur. Ullam debitis, inventore iusto maiores accusamus laborum, fugit enim saepe consequuntur praesentium provident natus soluta perspiciatis deleniti itaque est ex! Magni expedita quam similique rerum tenetur sunt sint nesciunt, libero, sed iusto delectus ut nostrum laboriosam molestias nemo quaerat. Incidunt sapiente recusandae, numquam placeat amet quo aperiam iste officiis velit, praesentium ducimus eos dolore labore reiciendis! Repudiandae in quis aliquam ipsum at officiis blanditiis inventore iure consequuntur dicta molestias optio ex voluptate veniam nostrum unde doloremque eveniet repellendus odio distinctio reprehenderit eum, veritatis nam! Assumenda vel, id quo obcaecati deserunt consequuntur quaerat, atque, facilis natus beatae debitis aspernatur impedit nesciunt dignissimos consequatur ratione ut sunt nisi! In consequuntur blanditiis dolorem tempore quidem iste aspernatur at beatae ea optio impedit culpa, quo consequatur dolor natus doloremque eius? Ullam nihil eligendi inventore aspernatur odio eveniet reiciendis animi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati non aut perferendis tempore voluptatum illum quidem culpa qui consectetur eaque laboriosam, pariatur mollitia deleniti voluptas debitis cupiditate et? Perferendis, consequatur!</p>
-                        </div>
+                        
                         <div>
-                            <p className="text-center mt-5" >
+                            <Link to="/event/signUp" >
+                            <p className="text-center" >
                                 <button className="btn btn-info w-100 ">Register</button>
                             </p>
+                            </Link>
                             <h3 className="text-center" >Attendees</h3>
                             {attendeeSquare}
 
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div  style={desc} >
+                            <h3 className="text-center" >Description</h3>
+                            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum consequuntur tempora corrupti dolores expedita sequi magnam quasi maiores neque eaque aliquam nulla, facilis pariatur, quidem eos ullam nisi dicta officia libero voluptate quibusdam dolor adipisci ut? Alias nihil, in iusto incidunt earum tenetur, nam quisquam ipsa corrupti sint et ad enim reprehenderit neque, dicta quis eveniet natus ducimus cupiditate molestias facilis sit repellat ex pariatur. Ullam debitis, inventore iusto maiores accusamus laborum, fugit enim saepe consequuntur praesentium provident natus soluta perspiciatis deleniti itaque est ex! Magni expedita quam similique rerum tenetur sunt sint nesciunt, libero, sed iusto delectus ut nostrum laboriosam molestias nemo quaerat. Incidunt sapiente recusandae, numquam placeat amet quo aperiam iste officiis velit, praesentium ducimus eos dolore labore reiciendis! Repudiandae in quis aliquam ipsum at officiis blanditiis inventore iure consequuntur dicta molestias optio ex voluptate veniam nostrum unde doloremque eveniet repellendus odio distinctio reprehenderit eum, veritatis nam! Assumenda vel, id quo obcaecati deserunt consequuntur quaerat, atque, facilis natus beatae debitis aspernatur impedit nesciunt dignissimos consequatur ratione ut sunt nisi! In consequuntur blanditiis dolorem tempore quidem iste aspernatur at beatae ea optio impedit culpa, quo consequatur dolor natus doloremque eius? Ullam nihil eligendi inventore aspernatur odio eveniet reiciendis animi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati non aut perferendis tempore voluptatum illum quidem culpa qui consectetur eaque laboriosam, pariatur mollitia deleniti voluptas debitis cupiditate et? Perferendis, consequatur!</p>
                         </div>
                     </div>
                 </div>
