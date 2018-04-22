bbe4c326c03bea320e6197fc2a711ebba45b9c31
Will Cheng <willchengsters@gmail.com>
2018-04-21 19:22:43 -0700

    sidebar content

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 import NavBar from './navigation/navBar';
-import Matching from './matching';
+import Matching from './matching/';
 
 const App = () => (
     <div>
