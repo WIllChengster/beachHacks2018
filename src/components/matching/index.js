9c5ae84ee4ae82caca279970fff80ac590179a80
Will Cheng <willchengsters@gmail.com>
2018-04-21 19:59:06 -0700

    matching component converted into functional component

--- src/components/matching/index.js
+++ src/components/matching/index.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import matchData from '../../data/matchingDummy'
 import DevCard from './devCard'
 
-export default () => {
+ class Matching extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+            devSelected:[]
+        }
+    }
 
-    const devCards = Object.keys(matchData).map( (item, index)=>{
-        return(
-            <DevCard key={index} user={matchData[item]} />
+    render(){
+        const devCards = Object.keys(matchData).map( (item, index)=>{
+            return(
+                <DevCard key={index} user={matchData[item]} />
 
-        )
-    } )
+            )
+        } )
 
-    return (
-        <div>
-            <h1>This is the matching page</h1>
-            <div className="row">
-                <div className="col-3 align-self-center ">
-                    <p>SIDEBAR CONTENT</p>
-                </div>
-                <div className="col">
-                    {devCards}
+        return (
+            <div>
+                <h1>This is the matching page</h1>
+                <div className="row">
+                    <div className="col-3 align-self-center jumbotron">
+                        <p className="text-center" >Developers Selected</p>
+                    </div>
+                    <div className="col">
+                        {devCards}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+export default Matching
