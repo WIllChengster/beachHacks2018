f8b989b9bf1948b686b94d62780dbe49cc66b546
Will Cheng <willchengsters@gmail.com>
2018-04-21 23:03:15 -0700

    value of input is determined by state

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -4,14 +4,40 @@ class Register extends Component{
     constructor(props){
         super(props)
         this.state={
-
+            title:'',
+            description:''
         }
     }
 
+    onTitleChange(event){
+        this.setState({
+            title:event.target.value
+        })
+    }
+
+    onDescriptionChange(event){
+        this.setState({
+            description: event.target.value
+        })
+    }
 
     render(){
         return(
             <div className="row">
+                <h1 className="text-center w-100">Register your Project</h1>
+                <div className="col jumbotron ">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="projectName">Title</label>
+                            <input onChange={this.onTitleChange.bind(this)} value={this.state.title} id="projectName" type="text" className="form-control" placeholder="Enter your project title" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="projectDesc">Brief Description</label>
+                            <textarea onChange={this.onDescriptionChange.bind(this)} value={this.state.description} id="projectDesc" className="form-control" placeholder="Enter a brief project description" rows="5" ></textarea>
+                        </div> 
+                        <button className="btn btn-primary" >Create your project!</button>
+                    </form>
+                </div>
             </div>
         )
     }
